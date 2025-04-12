ed5a10741be8dfd97bc2a4aceb53df7fdda3093e
William_Winhesta <sanyabobiev@gmail.com>
2025-04-12 13:34:20 +0300

    fix source basename

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,9 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./Layout/Layout.jsx";
 import MainPage from "../Page/MainPage/MainPage.jsx";
 import ProductsPage from "../Page/ProductsPage/ProductsPage.jsx";
+
 const Router = () => {
   return (
-    <BrowserRouter basename={"/krdligthpod"}>
+    <BrowserRouter basename="/krd_ligth_pod">
       <Routes >
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
